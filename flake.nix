d9b178da1c253b24596afd0b8eeb33ba4b62af62
crolbar <mni05@abv.bg>
2024-01-15 02:44:59 +0200

    flake.nix

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            pkg-config
+            openssl
+            (rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" ];
+            })
+          ];
+        };
+      }
+    );
+}
