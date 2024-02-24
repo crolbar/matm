8e0954f6848e49cfcbc688707d6061630573a4a4
crolbar <mni05@abv.bg>
2024-02-24 10:56:33 +0200

    fix double tap in selector in windows

--- selector/src/update.rs
+++ selector/src/update.rs
@@ -1,4 +1,4 @@
-use crossterm::event::{KeyCode, Event, self, MouseEventKind, KeyModifiers, MouseEvent};
+use crossterm::event::{self, Event, KeyCode, KeyEventKind, KeyModifiers, MouseEvent, MouseEventKind};
 use std::io::Result;
 
 use crate::{app::Selector, tui::Tui};
@@ -6,71 +6,72 @@ use crate::{app::Selector, tui::Tui};
 pub fn update(app: &mut Selector, tui: &mut Tui) -> Result<()> {
     if let Ok(event) = event::read() {
         if let Event::Key(key) = event {
-
-            if 
-                key.modifiers == KeyModifiers::CONTROL && key.code == KeyCode::Char('c') ||
-                key.modifiers == KeyModifiers::ALT && key.code == KeyCode::Char('q') ||
-                key.code == KeyCode::Esc
-            {
-                tui.exit()?;
-                std::process::exit(0)
-            }
-
-            // ctrl + backspace
-            if 
-                key.modifiers == KeyModifiers::CONTROL &&
-                key.code == KeyCode::Char('h')
-            {
-                app.search.needle.clear();
-
-                if !app.search.haystack.is_empty() {
-                    app.search.revert_items(&mut app.items)
+            if key.kind == KeyEventKind::Press {
+                if 
+                    key.modifiers == KeyModifiers::CONTROL && key.code == KeyCode::Char('c') ||
+                    key.modifiers == KeyModifiers::ALT && key.code == KeyCode::Char('q') ||
+                    key.code == KeyCode::Esc
+                {
+                    tui.exit()?;
+                    std::process::exit(0)
                 }
-            }
 
-            else if 
-                key.modifiers == KeyModifiers::ALT | KeyModifiers::SHIFT  ||
-                key.modifiers == KeyModifiers::ALT
-            {
-                if let KeyCode::Char(char) = key.code {
-                    match char {
-                        'j' => app.sel_next_item(),
-                        'k' => app.sel_prev_item(),
-                        'g' => app.table_state.select(Some(0)),
-                        'G' => app.table_state.select(Some(app.items.len().saturating_sub(1))),
+                // ctrl + backspace
+                if 
+                    key.modifiers == KeyModifiers::CONTROL &&
+                    key.code == KeyCode::Char('h')
+                {
+                    app.search.needle.clear();
+
+                    if !app.search.haystack.is_empty() {
+                        app.search.revert_items(&mut app.items)
+                    }
+                }
+
+                else if 
+                    key.modifiers == KeyModifiers::ALT | KeyModifiers::SHIFT  ||
+                    key.modifiers == KeyModifiers::ALT
+                {
+                    if let KeyCode::Char(char) = key.code {
+                        match char {
+                            'j' => app.sel_next_item(),
+                            'k' => app.sel_prev_item(),
+                            'g' => app.table_state.select(Some(0)),
+                            'G' => app.table_state.select(Some(app.items.len().saturating_sub(1))),
+                            _ => ()
+                        }
+                    }
+                } else {
+                    match key.code {
+                        KeyCode::Down | KeyCode::Tab => app.sel_next_item(),
+                        KeyCode::Up | KeyCode::BackTab => app.sel_prev_item(),
+                        KeyCode::Enter => { 
+                            app.exit = !app.items.is_empty()
+                        }
+
+                        KeyCode::PageUp | KeyCode::Home => app.table_state.select(Some(0)),
+                        KeyCode::PageDown | KeyCode::End => app.table_state.select(Some(app.items.len().saturating_sub(1))),
+
+                        KeyCode::Backspace => {
+                            app.search.pop_char();
+                            app.search.search_trough_origin_items(&mut app.items);
+
+                            if app.items.is_empty() {
+                                app.search.revert_items(&mut app.items)
+                            }
+                        },
+
+                        KeyCode::Char(char) => {
+                            app.search.push_char(char);
+                            app.search.search_trough_origin_items(&mut app.items);
+
+                            if app.table_state.selected().unwrap() >= app.items.len() {
+                                app.table_state.select(Some(0))
+                            }
+                        }
+
                         _ => ()
                     }
-                }
-            } else {
-                match key.code {
-                    KeyCode::Down | KeyCode::Tab => app.sel_next_item(),
-                    KeyCode::Up | KeyCode::BackTab => app.sel_prev_item(),
-                    KeyCode::Enter => { 
-                        app.exit = !app.items.is_empty()
-                    }
-
-                    KeyCode::PageUp | KeyCode::Home => app.table_state.select(Some(0)),
-                    KeyCode::PageDown | KeyCode::End => app.table_state.select(Some(app.items.len().saturating_sub(1))),
-
-                    KeyCode::Backspace => {
-                        app.search.pop_char();
-                        app.search.search_trough_origin_items(&mut app.items);
-
-                        if app.items.is_empty() {
-                            app.search.revert_items(&mut app.items)
-                        }
-                    },
-
-                    KeyCode::Char(char) => {
-                        app.search.push_char(char);
-                        app.search.search_trough_origin_items(&mut app.items);
-
-                        if app.table_state.selected().unwrap() >= app.items.len() {
-                            app.table_state.select(Some(0))
-                        }
-                    }
-
-                    _ => ()
                 }
             }
         } else
