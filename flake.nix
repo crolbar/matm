5bd3da7d4469b4b03408409b2d9214d172e742c9
crolbar <crolbar@crolbar.com>
2024-08-20 17:49:55 +0300

    nix flake overrite nixpkgs inputs

--- flake.nix
+++ flake.nix
@@ -1,8 +1,14 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
